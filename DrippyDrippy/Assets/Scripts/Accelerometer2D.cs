3dacb81b787eb05199b1048fe23c76bbc3473ec7
Springo <kevinmxia@gmail.com>
2014-08-16 19:25:05 -0400

    Accelerometer Script Signed-off-by: Springo <kevinmxia@gmail.com>

---
+++ DrippyDrippy/Assets/Scripts/Accelerometer2D.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Accelerometer2D : MonoBehaviour {
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void FixedUpdate () {
+		transform.Translate(Input.acceleration.x * Time.fixedDeltaTime * speed, 0, 0);
+	}
+}
