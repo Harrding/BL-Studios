230c7f6caee59b20ca1545334c4ff33e5306db5c
Harrding <harrisond2003@gmail.com>
2014-08-16 15:12:32 -0700

    Added points? kinda... Signed-off-by: Harrding <harrisond2003@gmail.com>

---
+++ DrippyDrippy/Assets/Scripts/PointsScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointsScript : MonoBehaviour {
+	public int points;
+	public float multiplier;
+
+	// Use this for initialization
+	void Start () {
+		points = 0;
+		multiplier = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		points += (int)(Time.deltaTime * multiplier);
+	}
+
+	void OnGUI() {
+		GUI.Label (new Rect (0, 0, 100f, 100f), "" + points);
+	}
+}
