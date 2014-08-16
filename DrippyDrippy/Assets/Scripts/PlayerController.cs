0501380eca5658c70efc0f0945ae33b356e709cf
Harrding <harrisond2003@gmail.com>
2014-08-16 16:53:53 -0700

    Signed-off-by: Harrding <harrisond2003@gmail.com>

---
+++ DrippyDrippy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public const KeyCode LEFT = KeyCode.A;
+	public const KeyCode RIGHT = KeyCode.D;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(LEFT)) {
+
+		}
+		if(Input.GetKey (RIGHT)) {
+
+		}
+	}
+}
