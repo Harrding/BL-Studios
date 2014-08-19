0a57df3049af3b1dfa4d3933d4ba6a38b185be57
Springo <kevinmxia@gmail.com>
2014-08-19 11:02:29 -0700

    Added Placeholder GUI stuff for testing.
    Signed-off-by: Springo <kevinmxia@gmail.com>

---
+++ DrippyDrippy/Assets/Scripts/GUI/MenuGUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuGUI : MonoBehaviour {
+	public GUISkin normal;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		GUI.BeginGroup (new Rect(0,0,Screen.width,Screen.height));
+		GUI.skin = normal;
+		GUI.Button (new Rect(Screen.width / 2 - Screen.width / 8,Screen.height / 2 - Screen.height / 16, Screen.width / 4, Screen.height / 8),"Play");
+		GUI.EndGroup ();
+	}
+}
