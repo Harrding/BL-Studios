13196833da470a6c721d76cde3344939a6373a9c
Angelica Feng <Angelica@Angelicas-MacBook-Pro.local>
2014-08-22 15:43:56 -0700

    Signed-off-by: Angelica Feng <Angelica@Angelicas-MacBook-Pro.local>

--- DrippyDrippy/Assets/Scripts/Player Controller/PlayerController.cs
+++ DrippyDrippy/Assets/Scripts/Player Controller/PlayerController.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+
 	public const KeyCode LEFT = KeyCode.A;
 	public const KeyCode RIGHT = KeyCode.D;
+	public float speed;
 
 	// Use this for initialization
 	void Start () {
@@ -13,10 +15,10 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey(LEFT)) {
-
+			transform.Translate(speed * Time.fixedDeltaTime * -1, 0, 0);
 		}
 		if(Input.GetKey (RIGHT)) {
-
+			transform.Translate(speed * Time.fixedDeltaTime, 0, 0);
 		}
 	}
 }
