37394122c58a3d1d61edafc4a720ef60de451159
Angelica Feng <Angelica@Angelicas-MacBook-Pro.local>
2014-08-22 16:22:19 -0700

    Signed-off-by: Angelica Feng <Angelica@Angelicas-MacBook-Pro.local>

--- DrippyDrippy/Assets/Scripts/GameController.cs
+++ DrippyDrippy/Assets/Scripts/GameController.cs
@@ -4,40 +4,24 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 
 	public GameObject player;
-	public GameObject obstacle;
-	public float dist;
-	public float width;
-	public float threshold, gap;
-
-	private bool shouldCreate;
+	public GUIStyle ggStyle;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
-		shouldCreate = true;
-		width = Screen.width / 320;
+
 	}
 
-	void Update () {
-		if (transform.position.y < threshold) {
-			GameObject clone = (GameObject)Instantiate (obstacle, transform.position + new Vector3(Random.Range (-1f * width, width), dist, -transform.position.z), transform.rotation);
-			threshold -= gap;
+
+	void OnGUI() {
+		if (player.GetComponent<PlayerController> ().gameOver) {
+			gameOver();
 		}
+	}
+
+	void gameOver() {
+		GUI.Label (new Rect (0, 0, Screen.width, Screen.height), "GAME OVER", ggStyle);
 	}
 	
-	// Update is called once per frame
-	/*void FixedUpdate () {
 
-		if(shouldCreate) {
-			shouldCreate = false;
-			StartCoroutine("createObstacle");
-		}
-
-	}
-
-	IEnumerator createObstacle() {
-		yield return new WaitForSeconds(1f);
-		GameObject clone = (GameObject)Instantiate (obstacle, player.transform.position + new Vector3(Random.Range (-1f * width, width), dist, 0), player.transform.rotation);
-		shouldCreate = true;
-	}*/
 }
--- DrippyDrippy/Assets/Scripts/Player Controller/PlayerController.cs
+++ DrippyDrippy/Assets/Scripts/Player Controller/PlayerController.cs
@@ -5,11 +5,16 @@ public class PlayerController : MonoBehaviour {
 
 	public const KeyCode LEFT = KeyCode.A;
 	public const KeyCode RIGHT = KeyCode.D;
+	public const float scaleValue = 100;
 	public float speed;
+	public float waterAmount;
+	public bool gameOver;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		waterAmount = scaleValue;
+		gameOver = false;
 	}
 	
 	// Update is called once per frame
@@ -20,5 +25,22 @@ public class PlayerController : MonoBehaviour {
 		if(Input.GetKey (RIGHT)) {
 			transform.Translate(speed * Time.fixedDeltaTime, 0, 0);
 		}
+
 	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if(collider.gameObject.CompareTag("NormalObs")){
+			changeWaterAmount( collider.gameObject.GetComponent<WaterChanger>().changeWaterValue());
+		}
+	}
+
+	void changeWaterAmount(float waterValue) {
+		waterAmount += waterValue;
+		transform.localScale = new Vector3 (waterAmount / scaleValue , waterAmount / scaleValue, transform.localScale.z);
+		if (waterAmount <= 0) {
+			gameOver = true;
+		}
+	}
+
+
 }
