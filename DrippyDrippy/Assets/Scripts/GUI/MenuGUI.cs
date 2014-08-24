09dcfe24129a4ce3d5b2a2168a7ce5e83d76001d
Springo <kevinmxia@gmail.com>
2014-08-23 17:32:38 -0700

    Added new button art from Hannah

--- DrippyDrippy/Assets/Scripts/GUI/MenuGUI.cs
+++ DrippyDrippy/Assets/Scripts/GUI/MenuGUI.cs
@@ -17,10 +17,12 @@ public class MenuGUI : MonoBehaviour {
 	void OnGUI () {
 		GUI.BeginGroup (new Rect(0,0,Screen.width,Screen.height));
 		GUI.skin = normal;
-		if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 3, Screen.height / 2 - Screen.height / 16, Screen.width * 2 / 3, Screen.height / 8), "Play")) {
+		GUIStyle buttonfont = new GUIStyle (GUI.skin.button);
+		buttonfont.fontSize = Screen.height / 18;
+		if (GUI.Button (new Rect (Screen.width / 2 - Screen.width / 3, Screen.height / 2 - Screen.height / 16, Screen.width * 2 / 3, Screen.height / 8), "Play", buttonfont)) {
 			Application.LoadLevel ("GameScene");
 		}
-		GUI.Button (new Rect(Screen.width / 2 - Screen.width / 3,Screen.height / 2 - Screen.height / 16 + Screen.height / 8 + Screen.height / 64, Screen.width * 2 / 3, Screen.height / 8),"High Scores");
+		GUI.Button (new Rect(Screen.width / 2 - Screen.width / 3,Screen.height / 2 - Screen.height / 16 + Screen.height / 8 + Screen.height / 64, Screen.width * 2 / 3, Screen.height / 8),"High Scores", buttonfont);
 		GUI.EndGroup ();
 	}
 }
