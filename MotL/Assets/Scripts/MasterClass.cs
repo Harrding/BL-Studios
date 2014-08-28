bc71cb1435dacfb33b32695934901d9bc49f3820
Harrding <harrisond2003@gmail.com>
2014-08-28 16:43:12 -0700

    Signed-off-by: Harrding <harrisond2003@gmail.com>

---
+++ MotL/Assets/Scripts/MasterClass.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class MasterClass {
+
+	/* Write down each food type and its index here
+	 * 0 = Wheat
+	 */
+	public static int NUM_FOOD = 1;
+
+}
---
+++ MotL/Assets/Scripts/Village.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Village : MonoBehaviour {
+
+	private int [] food = new int[MasterClass.NUM_FOOD];
+
+}
