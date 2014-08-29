6946cfe3d434a6a1e29d70f1123020a5535e118c
Harrding <harrisond2003@gmail.com>
2014-08-28 23:43:44 -0700

    Signed-off-by: Harrding <harrisond2003@gmail.com>

--- MotL/Assets/Scripts/MasterClass.cs
+++ MotL/Assets/Scripts/MasterClass.cs
@@ -11,17 +11,33 @@ public static class MasterClass {
 	 * 4 = Meat
 	 */
 	public static int NUM_FOOD = 5;
+	public static int WHEAT_ID = 0;
+	public static int FISH_ID = 1;
+	public static int POTATO_ID = 2;
+	public static int CABBAGE_ID = 3;
+	public static int MEAT_ID = 4;
 
 	/* Types of tools
 	 * 0 = Sword
 	 * 1 = Pickaxe
 	 * 2 = Hoe
+	 * 3 = Axe 
 	 */
 	public static int NUM_TOOLS = 3;
 
+	public static int SWORD_ID = 0;
+	public static int PICK_ID = 1;
+	public static int HOE_ID = 2;
+	public static int AXE_ID = 3;
+
+	/* Types of Medicine
+	 * 0 = Tonic
+	 */
+	public static int NUM_MEDS = 1;
+
 	// Starting values for each resource
 	public static int STARTING_FOOD = 500;
-	public static int STARTING_TOOLS= 2;
+	public static int STARTING_TOOLS= 100;
 	public static int STARTING_MEDICINE = 400;
 
 	// Method to get the price based on the scarcity of a certain food product
--- MotL/Assets/Scripts/Village.cs
+++ MotL/Assets/Scripts/Village.cs
@@ -3,24 +3,33 @@ using System.Collections;
 
 public abstract class Village {
 
+	// Resources
 	protected int [] food = new int[MasterClass.NUM_FOOD];
 	protected int MAX_FOOD;
 	protected int [] tools = new int[MasterClass.NUM_TOOLS];
 	protected int MAX_TOOLS;
+	protected int [] medicine = new int[MasterClass.NUM_MEDS];
+	protected int MAX_MEDS;
 
+	// Village Type
 	protected int type;
+
+	// Population
 	protected int population;
 	protected int POP_MAX;
 	protected float DEATH_RATE;
 	protected float BIRTH_RATE;
 
-	public string getType() {return type;}
+	// Getter Methods/ Default methods that exist already in each village
+	public int getType() {return type;}
 	public int getPopulation(){return population;}
 	public int [] getFood(){return food;}
 
+	// Abstract methods that are required in each village for each village to exist
 	public abstract void createResources();
 	public abstract void consumeFoodResources();
 	public abstract void consumeMedicineResources();
+	public abstract void useTools();
 }
 
 public class FarmingVillage : Village{
@@ -31,14 +40,34 @@ public class FarmingVillage : Village{
 		BIRTH_RATE = 1.0f;
 		population = 300;
 		setDefaultResources ();
+		setMaxResources ();
 	}
 	private void setDefaultResources() {
 		for (int i = 0; i < food.Length; i++) {
 			food[i] = MasterClass.STARTING_FOOD + 300;
 		}
-		for(int i = 0; i < tools.Length; i++) {
-			tools[i] = MasterClass.STARTING_TOOLS;
+
+		for(int i = 0; i < medicine.Length; i++) {
+			medicine[i] = MasterClass.STARTING_MEDICINE;
 		}
+		tools [MasterClass.HOE_ID] = MasterClass.STARTING_TOOLS;
+	}
+	private void setMaxResources() {
+		MAX_FOOD = 10000;
+		MAX_MEDS = 3000;
+		MAX_TOOLS = 1000;
+	}
+	public override void createResources() {
+		
+	}
+	public override void consumeFoodResources() {
+
+	}
+	public override void consumeMedicineResources(){
+
+	}
+	public override void useTools() {
+
 	}
 }
 
