b9287d7f95ae8a3f6fb409582635b15ad0ddf42c
Springo <kevinmxia@gmail.com>
2014-09-03 15:39:00 -0700

    Background Loop
    Signed-off-by: Springo <kevinmxia@gmail.com>

---
+++ DrippyDrippy/Assets/Scripts/SpawnNewBackground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnNewBackground : MonoBehaviour {
+	public GameObject background, cameraobj;
+	public bool spawned;
+	public int spawnlocation = -32;
+
+	// Use this for initialization
+	void Start () {
+		spawned = false;
+		cameraobj = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cameraobj.transform.position.y < transform.position.y && spawned == false) {
+			spawned = true;
+			GameObject newback = Instantiate (background, new Vector3(0,spawnlocation,transform.position.z),transform.rotation) as GameObject;
+			newback.GetComponent<SpawnNewBackground>().spawnlocation = spawnlocation - 16;
+		}
+	}
+}
